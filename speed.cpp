09187de5bf6db5a789a529b68a59316fbb2739b0
Preeti-Barua <40754414+Preeti-Barua@users.noreply.github.com>
2020-03-01 11:38:28 +0530

    speedlimit

---
+++ speed.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+using namespace std;
+int main()
+{    int  speed ,s;
+                 cout<<"enter the speed:";
+                 cin>>speed;
+                 if(speed <70)
+                 {
+                 	cout<<"okay";
+                 	
+                 }
+                 else if (speed>70)
+                 { 
+                    s=(speed-70)/5;
+                 	cout<<"points:"<<s<<endl;
+                    if(s>=12)
+                    {
+                    	cout<<"your point:"<<s<<"licence suspend"<<endl;
+                    	
+	}
+}
+                 
+                 
+   
+   	
+}
